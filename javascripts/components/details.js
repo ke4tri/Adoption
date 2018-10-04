de8c7512aefa7fa70fbd257afc7e44c4fbb8f2d3
Wayne Collier <ke4tri@gmail.com>
2018-10-04 08:39:13 -0500

    cards printing and just need style seperation I think

---
+++ javascripts/components/details.js
@@ -0,0 +1,32 @@
+// import {printToDom} from '../helpers/util.js';
+// import {charactersBuilder} from './characters.js';
+
+// const closeButtonEvent = () => {
+//   const closeButton = document.getElementById('close');
+//   closeButton.addEventListener('click', () => {
+//     charactersBuilder(getCharacterz());
+//   });
+// };
+
+// const detailsBuilder = (pets) => {
+//   let domString = '';
+//      domString +=   `<div class="card" style="width: 18rem;">`
+//      domString +=   `<div class="card-body">`
+//      domString +=       `<h3 class="card-link text-center">${pets[i]["name"]}</h3>`
+//      domString +=   `</div>`
+//      domString +=   `<img class="card-img-top" src="${pets[0]["imageUrl"]}">`
+//      domString +=   `<div class="card-body">`
+//      domString +=       `<h5 class="card-title text-center">${pets[0]["color"]}</h5>`
+//      domString +=       `<p class="${pets[0]["specialSkill"]}</p>`
+//      domString +=   `</div>`
+//      domString +=   `<div class="card-body">`
+//      domString +=       `<h5 class="card-link text-center ">${pets[0]["type"]}</h5>`
+//      domString +=   `</div>`
+//      domString +=   `</div>` 
+
+//   printToDom(domString);
+// //   closeButtonEvent();
+// };
+
+// export {detailsBuilder};
+
--- javascripts/components/petComponents.js
+++ javascripts/components/petComponents.js
@@ -1,11 +1,36 @@
 import { printToDom } from "../helpers/util.js";
 
-
 let pets = [];
 
 const setCharacters = (newArray) => {
-    pets = newArray;
-    console.log('this did it')
+  console.log('setCharacters')  
+  pets = newArray;
   };
 
-export {setCharacters}
+  const detailsBuilder = () => {
+    console.log('detailsbuilder')
+    let domString = '';
+     let i=0;
+    for(i=0;i<pets.length;i++){
+       domString +=   `<div class="card" style="width: 18rem;">`
+       domString +=   `<div class="card-body">`
+       domString +=       `<h3 class="card-link text-center">${pets[i]["name"]}</h3>`
+       domString +=   `</div>`
+       domString +=   `<img class="card-img-top" src="${pets[i]["imageUrl"]}">`
+       domString +=   `<div class="card-body">`
+       domString +=       `<h5 class="card-title text-center">${pets[i]["color"]}</h5>`
+       domString +=       `<p class="${pets[i]["specialSkill"]}</p>`
+       domString +=   `</div>`
+       domString +=   `<div class="card-body">`
+       domString +=       `<h5 class="card-link text-center ">${pets[i]["type"]}</h5>`
+       domString +=   `</div>`
+       domString +=   `</div>` 
+    }
+    printToDom(domString);
+  //   closeButtonEvent();
+  };
+  
+ 
+
+
+export {setCharacters, detailsBuilder}
