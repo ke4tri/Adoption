4746d687045cdbdf278b9e8023333719f6f07d59
Wayne Collier <ke4tri@gmail.com>
2018-10-04 10:30:50 -0500

    images fixed and loading, spacing inline bootstrap

--- javascripts/components/petComponents.js
+++ javascripts/components/petComponents.js
@@ -12,16 +12,16 @@ const setCharacters = (newArray) => {
     let domString = '';
      let i=0;
     for(i=0;i<pets.length;i++){
-       domString +=   `<div class="card" style="width: 18rem;">`
-       domString +=   `<div class="card-body">`
+       domString +=   `<div class="card  mb-3" style="width: 18rem;">`
+       domString +=   `<div class="card-body ">`
        domString +=       `<h3 class="card-link text-center">${pets[i]["name"]}</h3>`
        domString +=   `</div>`
        domString +=   `<img class="card-img-top" src="${pets[i]["imageUrl"]}">`
-       domString +=   `<div class="card-body">`
+       domString +=   `<div class="card-body ">`
        domString +=       `<h5 class="card-title text-center">${pets[i]["color"]}</h5>`
-       domString +=       `<p class="${pets[i]["specialSkill"]}</p>`
+       domString +=       `<p class="card-title text-center">${pets[i]["specialSkill"]}</p>`
        domString +=   `</div>`
-       domString +=   `<div class="card-body">`
+       domString +=   `<div class="card-body ">`
        domString +=       `<h5 class="card-link text-center ">${pets[i]["type"]}</h5>`
        domString +=   `</div>`
        domString +=   `</div>` 
